38f9d1194e90d2df34c66438180d19589f87b772
willder-root <willderaraujo_99_gds@hotmail.com>
2025-05-13 21:41:19 -0300

    adicionado aula 7

---
+++ aula7.py
@@ -0,0 +1,16 @@
+# variaveis
+nome_completo = 'willder araujo'
+soma_dois_mais_dois = 2 + 2
+print(nome_completo, soma_dois_mais_dois)
+
+valor = '1'
+valor_inteiro = int(valor)
+print(valor, type(valor))
+print(valor_inteiro, type(valor_inteiro))
+
+nome = 'Willder'
+idade = 25
+maior_de_idade = idade >= 18
+
+print("Nome:", nome, "Idade:", idade)
+print("É maior de idade:", maior_de_idade)
