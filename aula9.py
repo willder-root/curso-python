f3b64f66b251f4f30da14198cefc30f1bb76655a
willder-root <willderaraujo_99_gds@hotmail.com>
2025-05-13 21:53:52 -0300

    adicionando aula 9

---
+++ aula9.py
@@ -0,0 +1,22 @@
+# operadores aritiméticos
+
+adicao = 1 + 20
+print('Adição:', adicao)
+
+subtracao = 23 - 4
+print('Subtração:', subtracao)
+
+mutiplicacao = 2 * 3
+print('mutiplicação:', mutiplicacao)
+
+divisao = 10 / 5 # sempre retorna um valor do tipo float
+print('Divisão:', divisao)
+
+divisao_inteira = 10 // 2.2 # no resultado caso for um float sempre vai ter como casa decimal o zero
+print("Divisão inteira:", divisao_inteira)
+
+exponenciacao = 2 ** 10
+print("Exponenciação:", exponenciacao)
+
+modulo = 55 % 2 # resto da divisão
+print("Modulo:", modulo)
