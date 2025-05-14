cbf58e0b3cacf1fae0f32372e901723bc2369bef
willder-root <willderaraujo_99_gds@hotmail.com>
2025-05-13 21:46:14 -0300

    Adicionando aula 8

---
+++ aula8.py
@@ -0,0 +1,14 @@
+nome = 'willder'
+sobrenome = 'araujo'
+idade = 25
+ano_nascimento = 2000
+maior_idade = idade >= 18
+altura_metros = 1.82
+
+
+print("Nome:", nome)
+print("Sobrenome:", sobrenome)
+print("Idade:", idade)
+print("Ano de nascimento", ano_nascimento)
+print("É maior de idade?", maior_idade)
+print("Altura em metros:",altura_metros)
