d945c0d7d347d21a3f8e038974e90c24a6f20d5c
willder-root <willderaraujo_99_gds@hotmail.com>
2025-05-13 21:16:47 -0300

    adicionado aula 3

---
+++ aula3.py
@@ -0,0 +1,15 @@
+# usando strings
+
+# usando aspas simples
+print('olá, mundo')
+print('usando "aspas" duplas')
+
+#usando aspas duplas
+print("hello, world!")
+print("Usando 'aspas simples'")
+
+#Usando escape
+print("valor \" novo")
+
+#r
+print(r"Willder \" Araujo\"")
