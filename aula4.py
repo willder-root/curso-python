b9024da357bbbbbd8d6ef474665a2f2513658fbd
willder-root <willderaraujo_99_gds@hotmail.com>
2025-05-13 21:22:09 -0300

    adicionando aula 4

---
+++ aula4.py
@@ -0,0 +1,16 @@
+# tipos int e float
+
+# exemplos de valores do tipo int
+print(11)#int
+print(-11)#int
+print(0)
+
+#exemplos de valores do tipo float
+print(1.1, 10.11)
+print(0.0)
+print(-1.5)
+
+#verificar tipo do valor
+print(type('willder')) # str
+print(type(11)) # int
+print(type(1.1)) # float
