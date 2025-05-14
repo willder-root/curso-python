51de32f06e0ffbb5ff003020caea1bbb41346330
willder-root <willderaraujo_99_gds@hotmail.com>
2025-05-13 22:06:52 -0300

    adicionando aula 12

---
+++ aula12.py
@@ -0,0 +1,10 @@
+# calculando IMC
+
+nome = 'Willder Araujo'
+altura = 1.80 
+peso = 85
+imc = peso / (altura * altura)
+
+print(nome , 'tem', altura, 'de altura')
+print('pesa', peso, 'quilos e seu IMC é')
+print(imc)
