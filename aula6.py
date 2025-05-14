4e75c76c881827c384aaa3e427ba59921d23e9b4
willder-root <willderaraujo_99_gds@hotmail.com>
2025-05-13 21:32:25 -0300

    adicionando aula 6

---
+++ aula6.py
@@ -0,0 +1,13 @@
+# conversão de tipos com python
+# type convertion, typecasting, coercion
+print(1 + 1.0) # retorno float
+print(1 + 1) # retorno int
+print('a' + 'b') # retorno str
+
+print('novo ' * 8)
+print('1', type('1'))
+print(int('1'), type(int('1')))
+print(bool(''))
+print(bool('oi'))
+
+print(str(11) + ' novo')
