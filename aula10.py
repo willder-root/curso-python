b7dacd634bf45edda62d9d8732771332f1700368
willder-root <willderaraujo_99_gds@hotmail.com>
2025-05-13 21:59:24 -0300

    adicionado a aual 10

---
+++ aula10.py
@@ -0,0 +1,10 @@
+# concatenação (+) e repetição (*) com operadores aritiméticos
+
+concatenacao = 'A' + 'B' + 'C'
+print(concatenacao)
+
+a_dez_vezes = 'A' * 10
+tres_vezes_willder = 3 * 'willder'
+
+print(a_dez_vezes)
+print(tres_vezes_willder)
