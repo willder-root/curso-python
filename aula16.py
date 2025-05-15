def34ff522ad947042a20480d1899932dc083db2
willder-root <willderaraujo_99_gds@hotmail.com>
2025-05-15 06:55:50 -0300

    adicionando aula 16

---
+++ aula16.py
@@ -0,0 +1,11 @@
+# estrutura de condicoes
+# if / elif / else
+
+entrada = input('Você quer "entrar" ou "sair"?')
+
+if entrada == 'entrar':
+    print("Você entrou no sistema")
+elif entrada == "sair":
+    print("Você saiu do sistema")
+else:
+    print('Opção inválida!')
