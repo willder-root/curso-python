00ddf93ceae6f5a41f70300fa1b1e3fbcec02cc7
willder-root <willderaraujo_99_gds@hotmail.com>
2025-05-15 07:38:29 -0300

    adicionando aula17

---
+++ aula17.py
@@ -0,0 +1,18 @@
+# estrutura de condicoes
+# if / elif / else
+condicao1 = True
+condicao2 = True
+condicao3 = False
+condicao4 = False
+condicao5 = False
+condicao6 = False
+
+if condicao1:
+    print('Código para condição 1')
+elif condicao2:
+    print('Código da condição 2')
+else:
+    print("esse é o else do primeiro if")
+
+if 10 == 10:
+    print("Valores iguais")
