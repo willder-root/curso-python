c78108407003f28a1908f85a366b8a63b218f3bb
willder-root <willderaraujo_99_gds@hotmail.com>
2025-05-15 06:48:01 -0300

    adicionando aula 15

---
+++ aula15.py
@@ -0,0 +1,8 @@
+# usando a função input
+
+# nome = input('Qual o seu nome? ')
+# print(f"O seu nome é {nome}")
+numero_1 = int(input("Digite um número: "))
+numero_2 = int(input("Digite outro número: "))
+
+print(f"A soma dos númeos é: {numero_1 + numero_2}")
