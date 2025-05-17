10f121209494d5e5fc9d0a05424cee254d4e255d
willder-root <willderaraujo_99_gds@hotmail.com>
2025-05-17 09:57:29 -0300

    adicioanndo aula 20

---
+++ aula20.py
@@ -0,0 +1,7 @@
+primeiro_valor = input("Digite um Valor: ")
+segundo_valor = input("Digite outro valor: ")
+
+if primeiro_valor > segundo_valor:
+    print(f"{primeiro_valor=} é maior do que o {segundo_valor=}")
+else:
+    print(f"{segundo_valor=} é maior do que o {primeiro_valor=}")
