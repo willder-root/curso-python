0001bd8c09eb8dd08ba522436e67ac6b356debc2
willder-root <willderaraujo_99_gds@hotmail.com>
2025-05-17 09:49:41 -0300

    Adicioando aula 19

---
+++ aula19.py
@@ -0,0 +1,29 @@
+"""
+    Operadores relacionais
+OP          Siginificado              Exemplo(True)
+>           Maior                     2 > 1
+>=          Maior ou igual            2 >= 2
+<           Menor                     1 < 2
+<=          Menor ou igual            1 <= 1
+==          igual                     'a' == 'a'
+!=          Diferente                 'a' != 'b'
+"""
+
+
+
+maior = 2 > 2
+maior_ou_igual = 2 <= 2
+menor = 1 < 2
+menor_ou_igual = 1 <= 1
+igual = 'a' == 'a'
+diferente = 'a' != 'b'
+
+
+print(f"""
+    Maior: {maior}
+    Maior ou igual: {maior_ou_igual}
+    Menor: {menor}
+    Menor ou igual: {menor_ou_igual}
+    Igual: {igual}
+    Diferente: {diferente}
+""")
