09bc87cfac7738753ea8e8eb55d90421aac7ecef
willder-root <willderaraujo_99_gds@hotmail.com>
2025-05-17 12:14:00 -0300

    adicionando aula 24

---
+++ aula24.py
@@ -0,0 +1,11 @@
+# operadores in e not in
+# Strings que são iteráveis
+# 0, 1, 2, 3, 4, 5
+# W,i,l,l,d,e,r
+# -6,-5,-4,-5,-2,-1
+
+
+
+print('w' in 'willder')
+print('h' in 'willder')
+print('novo' not in 'acesso facil')
