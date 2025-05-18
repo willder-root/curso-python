ccee51282b307f9ea389b12bafeb6f8a5190a6ec
willder-root <willderaraujo_99_gds@hotmail.com>
2025-05-18 10:49:25 -0300

    adicionado a aula 41

---
+++ aula41.py
@@ -0,0 +1,14 @@
+"""
+    while/else
+"""
+
+variavel = 'Valor qualquer'
+
+i = 0 
+
+while i < len(variavel):
+    letra = variavel[i]
+    print(letra)
+    i += 1
+else:
+    print("O else foi executado")
