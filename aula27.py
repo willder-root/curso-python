493c1a8ca4256b4d89bfd6aec0cd88d4a75aff87
willder-root <willderaraujo_99_gds@hotmail.com>
2025-05-17 21:11:25 -0300

    adicionando aula 27

---
+++ aula27.py
@@ -0,0 +1,19 @@
+"""
+fATIAMENTO DE STRINGS 
+012345678
+olá mundo
+-987654321
+Fatiamento [inicio:fim:salto] [:]
+Obs.: A função len retorna a quantidade de caracteres da string
+"""
+
+variavel = 'Olá, mundo'
+print(variavel)
+print(variavel[1])
+print(variavel[-4])
+print(variavel[4:])
+print(variavel[:3])
+print(variavel[4::2])
+print(variavel[1:6:3])
+print(len(variavel))
+print(len(variavel[1:6:3]))
