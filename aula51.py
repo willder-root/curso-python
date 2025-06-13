54f2883d10fc1c6e6b68ab30cc5547a70cbb9b83
willder-root <willderaraujo_99_gds@hotmail.com>
2025-06-13 11:30:28 -0300

    adicionado aula 51

---
+++ aula51.py
@@ -0,0 +1,17 @@
+"""
+    introdução ao desempacotamento
+"""
+
+
+nomes = ['Willder', 'Joaquim', 'Maria']
+
+nome1, nome2, nome3 = nomes
+
+_, nome4, *_ = nomes
+
+print(nomes)
+print(nome1)
+print(nome2)
+print(nome3)
+print(nome4)
+print(_)
