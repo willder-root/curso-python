d374647bcd8353348c59b3f9277c2a3795aa47c5
willder-root <willderaraujo_99_gds@hotmail.com>
2025-06-13 15:34:28 -0300

    adicionando aula 58

---
+++ aula58.py
@@ -0,0 +1,12 @@
+"""
+Interpretador do Python
+
+python mod.py (executa o mod)
+python -u (unbuffered)
+python -m mod (lib mod com script)
+python -c 'cmd' (comando)
+python -i mod.py (interativo com mod)
+
+
+
+"""
